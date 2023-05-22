13ca38d42d7021cdb8138fc5cf41bae2f8b9ba9a
Amarachukwu Ojiakor <amyojiakor@gmail.com>
2023-05-22 22:32:11 +0100

    Add unit tests for NubanGeneratorServiceImpl
    This commit includes unit tests for the NubanGeneratorServiceImpl class. The tests cover both valid and invalid input scenarios.
    
    - Added a test case for valid input, `testGenerateNuban_ValidInput_ReturnsResponse()`, to verify that the generated Nuban is correct.
    - Added a test case for invalid serial number input, `testGenerateNuban_InvalidSerialNum_ReturnsErrorResponse()`, to check that an exception is thrown when the serial number is empty.
    - Added a test case for invalid bank code input, `testGenerateNuban_InvalidBanksCode_ReturnsErrorResponse()`, to ensure that an exception is thrown when an invalid bank code is provided.
    
    These tests help ensure the correctness of the generated Nuban.

--- src/test/java/com/amyojiakor/nubangeneratorapp/NubanGeneratorAppApplicationTests.java
+++ src/test/java/com/amyojiakor/nubangeneratorapp/NubanGeneratorAppApplicationTests.java
@@ -1,13 +1,63 @@
 package com.amyojiakor.nubangeneratorapp;
 
+import com.amyojiakor.nubangeneratorapp.models.payloads.NubanGeneratorPayload;
+import com.amyojiakor.nubangeneratorapp.models.payloads.NubanGeneratorResponse;
+import com.amyojiakor.nubangeneratorapp.repositories.NubanRepository;
+import com.amyojiakor.nubangeneratorapp.services.NubanGeneratorServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class NubanGeneratorAppApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Mock
+	private NubanRepository nubanRepository;
+
+	@InjectMocks
+	private NubanGeneratorServiceImpl nubanGeneratorService;
+
+	@BeforeEach
+	public void setUp() {
+		MockitoAnnotations.openMocks(this);
 	}
 
+	@Test
+	public void testGenerateNuban_ValidInput_ReturnsResponse() throws Exception {
+		String bankCode = "057";
+		String serialNum = "123456789";
+		NubanGeneratorPayload payload = new NubanGeneratorPayload(bankCode, serialNum);
+		String expectedNuban = "1234567899";
+
+		NubanGeneratorResponse response = nubanGeneratorService.generateNuban(payload);
+
+		Assertions.assertEquals(expectedNuban, response.generatedNuban());
+	}
+
+	@Test
+	public void testGenerateNuban_InvalidSerialNum_ReturnsErrorResponse() throws Exception {
+		// Arrange
+		String bankCode = "057";
+		String serialNum = ""; // Invalid input: empty serial number
+		NubanGeneratorPayload payload = new NubanGeneratorPayload(bankCode, serialNum);
+
+		Assertions.assertThrows(Exception.class, () -> {
+			nubanGeneratorService.generateNuban(payload);
+		});	}
+
+	@Test
+	public void testGenerateNuban_InvalidBanksCode_ReturnsErrorResponse() throws Exception {
+		// Arrange
+		String bankCode = "000"; // Invalid input: invalid bank code
+		String serialNum = "1234";
+		NubanGeneratorPayload payload = new NubanGeneratorPayload(bankCode, serialNum);
+
+		Assertions.assertThrows(Exception.class, () -> {
+			nubanGeneratorService.generateNuban(payload);
+		});	}
 }
